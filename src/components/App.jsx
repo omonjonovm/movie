3163fec073cc6eb14fa7917e39fc7aa3e5a5787d
Murodjon <example@gmail.com>
2024-06-07 10:09:58 +0500

    The data is handled by the api using the json placeholder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useState } from "react";
+import { useState, useEffect } from "react";
 import Info from "./Info";
 import Search from "./Search";
 import Filter from "./Filter";
@@ -6,46 +6,65 @@ import MovieList from "./MovieList";
 import AddForm from "./Add";
 import { v4 as uuidv4 } from 'uuid';
 
-const App  = () => {
-    const [data,setData] = useState(arr)
-    const [term,setTerm] = useState('')
-    const [filter,setFilter] = useState('all')
+const App = () => {
+    const [data, setData] = useState([])
+    const [term, setTerm] = useState('')
+    const [filter, setFilter] = useState('all')
+    const [isLoading, setisLoading] = useState(false)
     const onDelete = id => {
-        const newArr  = data.filter(c => c.id !== id)
+        const newArr = data.filter(c => c.id !== id)
         setData(newArr)
     }
     const addForm = item => {
         const newItem = { name: item.name, viewers: item.viewers, id: uuidv4(), favourite: false, like: false }
-        const  newArr = [...data, newItem]
+        const newArr = [...data, newItem]
         setData(newArr)
     }
     const onToggleProp = (id, prop) => {
         const newArr = data.map(item => {
-            if(item.id === id ) {
-                return{...item,[prop]: !item[prop]}
+            if (item.id === id) {
+                return { ...item, [prop]: !item[prop] }
             }
             return item
         })
         setData(newArr)
     }
-    const searchHandle = (arr,term) => {
-        if(term === 0){
+    const searchHandle = (arr, term) => {
+        if (term === 0) {
             return arr
         }
         return arr.filter(item => item.name.toLowerCase().indexOf(term) > -1)
     }
-    const   filterHandle = (arr,filter) => {
-        switch(filter) {
+    const filterHandle = (arr, filter) => {
+        switch (filter) {
             case 'popular':
-            return arr.filter(c => c.like)
+                return arr.filter(c => c.like)
             case 'mostViewers':
-                return arr.filter(c => c.viewers > 900)
+                return arr.filter(c => c.viewers > 30)
             default:
                 return arr
         }
     }
-    const updateTermHandle = term =>  setTerm(term)
+    const updateTermHandle = term => setTerm(term)
     const updateFilterHandle = filter => setFilter(filter)
+    useEffect(() => {
+        setisLoading(true)
+        fetch('https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5')
+            .then(response => response.json())
+            .then(json => {
+                const newArr = json.map(item => (
+                    {
+                        name: item.title,
+                        id: item.id,
+                        viewers:item.id*10,
+                        favourite:false,
+                        like:false
+                    }))
+                setData(newArr);
+            })
+            .catch(err => console.log(err))
+            .finally(() => setisLoading(false))
+    },[])
     return (
         <div className="app font-monospace ">
             <div className="content">
@@ -54,16 +73,11 @@ const App  = () => {
                     <Search updateTermHandle={updateTermHandle} />
                     <Filter filter={filter} updateFilterHandle={updateFilterHandle} />
                 </div>
-                <MovieList onToggleProp={onToggleProp}  data={filterHandle(searchHandle(data,term),filter)} onDelete={onDelete} />
+                {isLoading && "Loading..."}
+                <MovieList onToggleProp={onToggleProp} data={filterHandle(searchHandle(data, term), filter)} onDelete={onDelete} />
                 <AddForm addForm={addForm} />
             </div>
         </div>
     )
 }
 export default App;
-
-const arr = [
-    { name: 'Empire of Osman', viewers: 911, favourite: false, like: false, id: 1 },
-    { name: 'Breaking Bad', viewers: 890, favourite: false, like: false, id: 2 },
-    { name: 'Prison Break', viewers: 1203, favourite: false, like: false, id: 3 },
-]
